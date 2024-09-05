14f357edba63c6e30b7bbc68c07f0bbc9aece1dd
Jadav Srikar <136553839+srikarrj@users.noreply.github.com>
2024-09-05 21:08:52 +0530

    Update ecs-cluster.tf

--- ecs-cluster.tf
+++ ecs-cluster.tf
@@ -1,40 +1,23 @@
-
-  family                   = "medusa-task"
-  cpu                      = "512"
-  memory                   = "1024"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-  task_role_arn            = aws_iam_role.ecs_task_execution_role.arn
-
-  container_definitions = jsonencode([{
-    name      = "medusa-container"
-    image     = aws_ecr_repository.medusa_repo.repository_url
-    cpu       = 256
-    memory    = 512
-    essential = true
-    portMappings = [{
-      containerPort = 9000
-      hostPort      = 9000
-      protocol      = "tcp"
-    }]
-    environment = [
-      {
-        name  = "DATABASE_URL"
-        value = aws_secretsmanager_secret.medusa_db_secret.arn
-      }
-    ]
-  }])
+resource "aws_ecs_cluster" "medusa_cluster" {
+  name = "medusa-cluster"
 }
 
-  cluster         = aws_ecs_cluster.medusa_cluster.id
-  task_definition = aws_ecs_task_definition.medusa_task.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"
+resource "aws_ecs_task_definition" "medusa_task" {
+  family                   = "medusa-task"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  network_mode             = "awsvpc"
 
-  network_configuration {
-    subnets         = aws_subnet.public_subnet[*].id
-    security_groups = [aws_security_group.ecs_sg.id]
-    assign_public_ip = true
-  }
+  container_definitions = jsonencode([
+    {
+      name      = "medusa-container"
+      image     = aws_ecr_repository.medusa_repo.repository_url
+      essential = true
+      portMappings = [{
+        containerPort = 80
+        hostPort      = 80
+      }]
+    }
+  ])
 }
