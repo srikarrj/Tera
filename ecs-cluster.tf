2d352116c9239a8a2ecb9a2b40e9c54e88af182d
Jadav Srikar <136553839+srikarrj@users.noreply.github.com>
2024-09-05 21:37:39 +0530

    Update ecs-cluster.tf

--- ecs-cluster.tf
+++ ecs-cluster.tf
@@ -1,4 +1,29 @@
+resource "aws_ecs_cluster" "medusa_cluster" {
+  name = "medusa-cluster"
+}
 
+resource "aws_ecs_task_definition" "medusa_task" {
+  family                   = "medusa-task"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  network_mode             = "awsvpc"
+
+  container_definitions = jsonencode([
+    {
+      name      = "medusa-container"
+      image     = aws_ecr_repository.medusa_repo.repository_url
+      essential = true
+      portMappings = [{
+        containerPort = 80
+        hostPort      = 80
+      }]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "medusa_service" {
+  name            = "medusa-service"
   cluster         = aws_ecs_cluster.medusa_cluster.id
   task_definition = aws_ecs_task_definition.medusa_task.arn
   desired_count   = 1
